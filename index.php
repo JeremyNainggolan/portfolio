307f8d9073676c4bd4546dcf295b48ef2ffb36c9
Jeremy Mart Valentino Nainggolan <p.nainggolan102@gmail.com>
2024-07-22 09:07:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Mart Jeremiah | Portfolio</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+
+<body class="index-page">
+    <header id="header" class="header d-flex align-items-center sticky-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+            <a href="index.php" class="logo d-flex align-items-center">
+                <h1 class="sitename">Mart Jeremiah</h1>
+            </a>
+
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="#home" class="active">Home<br></a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#resume">Resume</a></li>
+                    <li><a href="./php/cv.php">Check out my CV</a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+
+
+        <section id="home" class="hero section dark-background">
+            <img src="assets/img/home.jpg" alt="" data-aos="fade-in">
+            <div class="container d-flex flex-column align-items-center justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
+                <h2>Jeremy Nainggolan</h2>
+                <p><span class="typed" data-typed-items="Programmer, Photographer"></span></p>
+            </div>
+        </section>
+
+        <section id="about" class="about section">
+            <div class="container" data-aos="fade-up" data-aos-delay="100">
+                <div class="row gy-4">
+                    <div class="col-md-5">
+                        <div class="row justify-content-between gy-4">
+                            <div class="col-lg-4">
+                                <img src="assets/img/profile.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="col-lg-8 about-info">
+                                <p><strong>Name: </strong> <span>Jeremy Mart Valentino Nainggolan</span></p>
+                                <p><strong>Profile: </strong> <span>Programmer</span></p>
+                                <p><strong>Email: </strong> <span>jeremy.nainggolann@gmail.com</span></p>
+                                <p><strong>Phone: </strong> <span>(+62) 851-7167-9411</span></p>
+                            </div>
+                        </div>
+
+                        <div class="skills-content skills-animation">
+
+                            <h5>Skills</h5>
+
+                            <div class="progress">
+                                <span class="skill"><span>JAVA</span> <i class="val">85%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill"><span>HTML | CSS | JAVASCRIPT</span> <i class="val">80%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill"><span>MYSQL</span> <i class="val">70%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill"><span>POSTGRESQL</span> <i class="val">70%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill"><span>PHP</span> <i class="val">40%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill"><span>C</span> <i class="val">70%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-7">
+                        <div class="about-me">
+                            <h4>About me</h4>
+                            <p>
+                                As a dedicated information systems student at Del Institute of Technology, I'm driven to explore the
+                                cutting-edge worlds of programming and photography. My passion lies in solving complex technology-related problems,
+                                and I'm committed to delivering high-quality results, even when faced with challenges that push me out of my
+                                comfort zone. This relentless pursuit of excellence has taught me the value of perseverance and the importance
+                                of learning from my mistakes.
+                            </p>
+                            <p>
+                                Through my involvement in various college organizations and school groups, I've developed strong communication skills,
+                                both individually and as part of a team. I'm confident in my ability to articulate my ideas clearly and effectively,
+                                and I thrive in environments where collaboration and creative problem-solving are encouraged.
+                            </p>
+                            <p>
+                                Some key strengths that set me apart include:
+                            <ul>
+                                <li>
+                                    My ability to think critically and approach problems from multiple angles
+                                </li>
+                                <li>
+                                    My strong analytical skills, which enable me to identify and troubleshoot issues efficiently
+                                </li>
+                                <li>
+                                    My excellent communication skills, both written and verbal
+                                </li>
+                                <li>
+                                    My willingness to take calculated risks and learn from my mistakes
+                                </li>
+                            </ul>
+                            I'm excited to bring my skills and enthusiasm to a dynamic organization where I can contribute
+                            to innovative projects and continue to grow as a professional.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="resume" class="resume section">
+
+            <div class="container section-title" data-aos="fade-up">
+                <h2>Resume</h2>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                        <h3 class="resume-title">Education</h3>
+                        <div class="resume-item">
+                            <h4>Bachelor of Information Systems </h4>
+                            <h5>2022 - 2026</h5>
+                            <p><em>Del Institute of Technology, Laguboti, North Sumatra</em></p>
+                        </div>
+
+                        <h3 class="resume-title">Volunteer</h3>
+                        <div class="resume-item">
+                            <h4>Study Orientation and Campus Introduction</h4>
+                            <h5>2023</h5>
+                            <p><em>Del Institute of Technology, Laguboti, North Sumatra </em></p>
+                            <p>Head of Documentation Division</p>
+                            <ul>
+                                <li>Leading a team of volunteers in organizing study orientation and campus introduction events</li>
+                                <li>Coordinating with other divisions to ensure smooth execution of the events</li>
+                                <li>Managing documentation and record-keeping for the events</li>
+                            </ul>
+                        </div>
+
+                        <h3 class="resume-title">Organization</h3>
+                        <div class="resume-item">
+                            <h4>Information Systems Student Association</h4>
+                            <h5>2023 - Present</h5>
+                            <p><em>Del Institute of Technology, Laguboti, North Sumatra </em></p>
+                            <p>Member of the Education Division</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+
+                        <h3 class="resume-title">Experiences</h3>
+                        <div class="resume-item">
+                            <h4>Teaching Assistant for Visual Programming Course</h4>
+                            <h5>September, 2023 - December, 2023</h5>
+                            <p><em>Del Institute of Technology, Laguboti, North Sumatra</em></p>
+                            <ul>
+                                <li>Encouraged student participation, creating an open and collaborative learning environment</li>
+                                <li>Ensured that the study materials were accessible and comprehensible for students.</li>
+                                <li>Assisted in the preparation of teaching materials and the grading of assignments and exams.</li>
+                            </ul>
+                        </div>
+
+                        <div class="resume-item">
+                            <h4>Teaching Assistant for Computer of Architecture Course</h4>
+                            <h5>February, 2024 - May, 2024</h5>
+                            <p><em>Del Institute of Technology, Laguboti, North Sumatra</em></p>
+                            <ul>
+                                <li>Assisted in conducting practical sessions and providing guidance to students</li>
+                                <li>Assisted in organizing and coordinating course-related events and activities</li>
+                                <li>Graded assignments and exams, providing constructive feedback to students</li>
+                            </ul>
+                        </div>
+
+                        <div class="resume-item">
+                            <h4>Student Internship</h4>
+                            <h5>June, 2024 - August, 2024</h5>
+                            <p><em>PT. Younexa Inti Materials, Cikarang, West Java</em></p>
+                            <ul>
+                                <li>Collaborated with a team of developers to implement new features and fix bugs</li>
+                                <li>Performed testing and debugging to ensure the application's functionality and performance</li>
+                                <li>Assisted in documenting the application's code and functionality</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer id="footer" class="footer accent-background">
+        <div class="container">
+            <div class="copyright text-center ">
+                <p>© <span>Copyright</span> <strong class="px-1 sitename">Mart Jeremiah</strong> <span>All Rights Reserved</span></p>
+            </div>
+            <div class="social-links d-flex justify-content-center">
+                <a href="https://www.linkedin.com/in/jeremy-nainggolan" target="_blank"><i class="bi bi-linkedin flex-shrink-0"></i></a>
+                <a href="mailto:jeremy.nainggolann@gmail.com" target="_blank"><i class="bi bi-envelope flex-shrink-0"></i></a>
+                <a href="https://github.com/JeremyNainggolan" target="_blank"><i class="bi bi-github"></i></a>
+                <a href="https://www.instagram.com/mart.jeremiah/" target="_blank"><i class="bi bi-instagram"></i></a>
+                <a href="https://wa.me/6285171679411" target="_blank"><i class="bi bi-whatsapp"></i></a>
+            </div>
+        </div>
+    </footer>
+
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/typed.js/typed.umd.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ php/cv.php
@@ -0,0 +1,18 @@
+<?php
+  
+// Store the file name into variable
+$file = '../document/resume.pdf';
+$filename = 'Resume';
+  
+// Header content type
+header('Content-type: application/pdf');
+  
+header('Content-Disposition: inline; filename="' . $filename . '"');
+  
+header('Content-Transfer-Encoding: binary');
+  
+header('Accept-Ranges: bytes');
+  
+// Read the file
+@readfile($file);
+  
